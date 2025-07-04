300acb01d1591b90d1e5767a82a78541f812336a
Weihnachtsbaum <140716472+Weihnachtsbaum@users.noreply.github.com>
2025-07-04 21:42:30 +0200

    Add music gen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::fs;
+
+use markov::Chain;
+use midly::Smf;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read("input.mid").expect("Could not read MIDI");
+    let smf = Smf::parse(&input).expect("Could not parse MIDI");
+    println!("{:?}", smf.header);
+    println!("Tracks: {}", smf.tracks.len());
+    let mut chain = Chain::new();
+    let mut out = Smf::new(smf.header);
+    for track in smf.tracks {
+        chain.feed(track);
+        out.tracks.push(chain.generate());
+    }
+    out.save("out.mid").expect("Could not save MIDI");
+    println!("Saved output to `out.mid`");
 }
